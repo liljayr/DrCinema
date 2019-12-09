b9f9ba085d08608bc232698eff01246842baf140
liljag18 <liljag18@ru.is>
2019-12-09 09:59:35 +0000

    redux has begun

--- DrCinema/App.js
+++ DrCinema/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import AppContainer from './src/routes';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
 
 
 export default function App() {
   return (
-    <AppContainer />
+    <Provider store={createStore(reducers)}>
+      <AppContainer />
+    </Provider>
   );
 }
