25538c31be07b87a1993789a9c4c9319811beedb
liljag18 <liljag18@ru.is>
2019-12-09 11:23:01 +0000

    merging stuff

--- DrCinema/src/components/CinemaList/index.jsx
+++ DrCinema/src/components/CinemaList/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, FlatList } from 'react-native';
+import ListItem from '../CinemaItem/index'
+import { connect } from 'react-redux';
 
 const CinemaList =({
-  contacts,
+  cinemas,
 }) => (
   <View>
-    <FlatList>
+    <FlatList
       numColumns={1}
-      data={contacts}
+      data={cinemas}
       renderItem={({
         item: {
-          id, name, phone, photo,
+          id, name, website,
         },
       }) => (
         <ListItem
@@ -22,4 +24,14 @@ const CinemaList =({
       )}
     />
   </View>
-)
+);
+
+ContactList.propTypes = {
+  cinemas: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    website: PropTypes.string.isRequired,
+  })).isRequired,
+}
+
+export default connect(mapStateToProps)(CinemaList);
