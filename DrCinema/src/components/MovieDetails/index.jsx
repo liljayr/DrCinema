8f471f4a8e6ad13a9ee13df8bae3a3f57a720a22
Azuki <sigvaldi22@gmail.com>
2019-12-11 18:00:08 +0000

    Display showtimes added in MovieDetails !Requires testing!

--- DrCinema/src/components/MovieDetails/index.jsx
+++ DrCinema/src/components/MovieDetails/index.jsx
@@ -5,7 +5,7 @@ import { Text } from 'react-native-elements';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
-const MovieDetails = ({ movie }) => (
+const MovieDetails = ({ movie, cinemaId }) => (
   <View>
     <View>
       <Text h1 style={styles.title}>{movie.name}</Text>
@@ -33,14 +33,24 @@ const MovieDetails = ({ movie }) => (
       {movie.genre.map((genre) => <Text>{genre.name}</Text>)}
     </View>
     <View>
-      <FlatList>
-        data={movie.showtimes}
-      </FlatList>
+      <FlatList
+        numColumns={1}
+        data={movie.showtimes.filter((item) => item.cinema.id === cinemaId)}
+        renderItem={({
+          item: {
+            showtimes, purchase_url,
+          },
+        }) => (
+          <DisplayShowtimes time={showtimes} url={purchase_url} />
+        )}
+        keyExtractor={(list) => list.id.toString()}
+      />
     </View>
   </View>
 );
 
 MovieDetails.propTypes = {
+  cinemaId: PropTypes.number.isRequired,
   movie: PropTypes.shape({
     name: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
@@ -55,7 +65,7 @@ MovieDetails.propTypes = {
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
       }).isRequired,
-      showtimes: PropTypes.arrayOf(PropTypes.shape({
+      schedule: PropTypes.arrayOf(PropTypes.shape({
         time: PropTypes.string.isRequired,
         purchase_url: PropTypes.toString.isRequired,
       })).isRequired,
@@ -63,3 +73,16 @@ MovieDetails.propTypes = {
   }).isRequired,
 };
 export default MovieDetails;
+
+function DisplayShowtimes({ time, url }) {
+  return (
+    <View>
+      <View>
+        <Text>{time}</Text>
+      </View>
+      <View>
+        <Text>{url}</Text>
+      </View>
+    </View>
+  );
+}
