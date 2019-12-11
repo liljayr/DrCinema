4a9a47f1cb6c30fe4c49c3da4123366f1a613d67
liljag18 <liljag18@ru.is>
2019-12-11 15:20:05 +0000

    error in API, cant get data for movies

--- DrCinema/src/views/Cinema/index.jsx
+++ DrCinema/src/views/Cinema/index.jsx
@@ -27,6 +27,8 @@ class Cinema extends React.Component {
     if (movies.length < 1) {
       initializeMoviesState(await getMovies());
     }
+    console.log('movies');
+    console.log(movies);
     const { cinemas } = this.props;
     const { cinemaId } = this.state;
     const tempMovies = [];
@@ -37,16 +39,18 @@ class Cinema extends React.Component {
     }
     for (let i = 0; i < movies.length; i += i) {
       const { showtimes } = movies[i];
+      console.log('showtimes');
+      console.log(showtimes);
       for (let j = 0; j < showtimes.length; j += j) {
         const ID = showtimes[i].cinema.id;
-        console.log(ID);
         if (cinemaId === ID) {
           tempMovies.push(movies[i]);
         }
       }
     }
     this.setState({ cinemaMovies: tempMovies });
-    console.log(cinemaMovies);
+    console.log('cinemamovies array');
+    console.log(this.state.cinemaMovies);
   // TODO: get Theater:  Name, Description, Complete address, Phone, Website
   // TODO: get Movies
   }
